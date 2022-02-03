e4e7a86e091b73b6b5893a936291a84eb00a0318
Boris Salleg <borissalleg@gmail.com>
2022-02-03 10:57:24 -0500

    primer commit

---
+++ src/main/java/com/example/demo/controller/homeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class homeController {
+
+    @GetMapping(value = "/")
+    public String home() {
+
+        return "HOME";
+    }
+
+    @GetMapping(value = "/home")
+    public String home1() {
+
+        return "HOME 1";
+    }
+}
